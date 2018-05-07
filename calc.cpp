dd623fd7397fe2acf171300965efafd2e0bc30f3
John M. Dlugosz <john.dlugosz@vsee.com>
2018-05-07 17:15:33 -0500

    Initial version

---
+++ calc.cpp
@@ -0,0 +1,193 @@
+#include <iostream>
+#include "d3/twostep.h"
+#include "d3/overflow.h"
+#include "calc1.h"
+
+namespace D3 = Dlugosz::d3;
+using namespace D3::twostep;
+using D3::sSize;
+
+using std::string_view;
+using std::string;
+using std::optional;
+
+
+
+// ===== variable storage
+
+auto EqEv_t::get_value(string_view name) const -> optional<Value_type>
+{
+    string key { name };
+    auto it= variables.find(key);
+    optional<Value_type> retval;
+    if (it != End(variables))  retval= it->second;
+    return retval;
+}
+
+
+void EqEv_t::set_value (string_view name, const optional<Value_type>& val)
+{
+    string key { name };
+    if (!val)  variables.erase(key);
+    variables.insert_or_assign (key, *val);
+}
+
+
+// there was no specification as to being case-insensitive or normalized,
+// so take it as strict exact chars.  This function is where the raw
+// spelling can be massaged into a canonocal form (e.g. case folded).
+string EqEv_t::normalize_identifier (const char* start, int len)
+{
+	return {start, start+len};
+}
+
+
+// ===== parser
+
+namespace {
+
+struct prim_parse_error {
+    const char* pos;
+    int errcode;
+};
+
+void raise_error (string_view input, int code)
+{
+    prim_parse_error Err { Data(input), code };
+    throw Err;
+}
+
+}
+
+
+void EqEv_t::skip_ws (string_view& s)
+{
+	while (!s.empty() && isspace(static_cast<unsigned char>(s.front())))  s.remove_prefix(1);
+}
+
+
+void EqEv_t::read_required (char c, string_view& s)
+{
+	if (Size(s) > 0 && s.front() == c)  {
+		s.remove_prefix(1);
+        skip_ws (s);
+        return;
+	}
+	throw ("needed specified character");
+}
+
+
+
+optional<string> EqEv_t::read_identifier (string_view& input)
+{
+	optional<string> retval;
+	int len= 0;
+	int Max = sSize(input);
+	for (; len < Max; ++len)  if (!isalpha(static_cast<const unsigned char>(input[len]))) break;
+	if (len==0)  return retval;
+	retval.emplace(normalize_identifier(&(*Begin(input)),len));
+	// after all went well, update the input range to take these chars
+	input.remove_prefix(len);
+    skip_ws(input);
+	return retval;
+}
+
+
+auto EqEv_t::read_value (string_view& input) -> optional<Value_type>
+{
+    ;
+    // variable | number
+    optional<Value_type> num= read_number(input);
+    if (!num) {
+        auto var= read_identifier(input);
+        if (var)  num= get_value(*var);
+    }
+    return num;
+}
+
+
+auto EqEv_t::read_number(string_view& input) -> optional<Value_type>
+{
+    optional<Value_type> retval;
+    int len= 0;
+    int Max = sSize(input);
+    for (; len < Max; ++len)  if (!isdigit(static_cast<const unsigned char>(input[len]))) break;
+    if (len==0)  return retval;
+    string numword (Begin(input), Begin(input)+len);  // Boost cpp_num doesn't know about string_view
+    retval.emplace(numword);
+    input.remove_prefix(len);
+    skip_ws(input);
+    return retval;
+}
+
+
+auto EqEv_t::read_binop (string_view& input) -> optional<op_enum>
+{
+    optional<op_enum> retval;
+    if (Size(input) > 0) {
+        char c= input.front();
+        // I only have one operator, and it is single-character.
+        if (c == '+') {
+            retval = OP_plus;
+            input.remove_prefix(1);
+            skip_ws (input);
+        }
+    }
+    return retval;
+}
+
+
+auto EqEv_t::read_terms (string_view & original_input) -> optional<Value_type>
+{
+    optional<Value_type> retval;
+    string_view input = original_input;
+
+    auto nextval = read_value (input);
+    if (!nextval)  return retval;
+    Value_type total = *nextval;
+    for (;;)
+    {
+        auto op = read_binop (input);
+        if (!op)  break;
+        nextval = read_value (input);
+        if (!nextval)  throw ("missing value");
+        // I only have one operator, addition
+        total += *nextval;
+    }
+    retval= std::move(total);
+    original_input= input;
+	return retval;
+}
+
+
+string EqEv_t::eval (string_view s)
+{
+	skip_ws (s);
+	auto LHS= read_identifier(s);
+	read_required ('=', s);
+	auto terms_result= read_terms(s);
+    if (!s.empty())  throw "extra stuff on line";
+    set_value (*LHS, terms_result);
+    return *LHS;
+}
+
+
+void test (EqEv_t& calc, string_view s)
+{
+    try {
+        string varname= calc.eval (s);
+        std::cout << "evaluated: " << varname << " = " << *calc.get_value(varname) << '\n';
+    }
+    catch (const char* msg) {
+        // temporary troubleshooting only!
+        std::cout << " <<< error: " << msg << '\n';
+    }
+}
+
+int main()
+{
+	EqEv_t calc;
+	test (calc, "  left = 5");
+	test (calc, "right=6  ");
+	test (calc, "total =left +right");
+}
---
+++ calc1.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include <string_view>
+#include <optional>
+#include <boost/multiprecision/cpp_int.hpp>
+#include <boost/container/flat_map.hpp>
+
+/*
+
+equation ::= variable '=' terms
+variable ::= [:isalpha:]+   // no case or other normalization specified
+terms ::= value { binop value }*
+value ::= variable | number
+number ::= [:isdigit:]+   // positive only; no negative sign
+
+*/
+
+
+
+
+class EqEv_t {
+public:
+    using Value_type = boost::multiprecision::cpp_int;
+    enum op_enum { OP_plus=1 };
+private:
+    boost::container::flat_map<std::string,Value_type> variables;
+	void skip_ws (std::string_view&);
+	std::optional<std::string> read_identifier (std::string_view&);
+	std::string normalize_identifier (const char* start, int len);
+	void read_required (char c, std::string_view&);
+	std::optional<Value_type> read_terms (std::string_view&);
+	std::optional<Value_type> read_value (std::string_view&);
+    std::optional<op_enum> read_binop (std::string_view&);
+    std::optional<Value_type> read_number (std::string_view&);
+public:
+	std::string eval (std::string_view);
+    std::optional<Value_type> get_value (std::string_view) const;
+    void set_value (std::string_view, const std::optional<Value_type>&);
+};
